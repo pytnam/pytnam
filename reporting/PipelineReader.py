7b8a0036dcbb533d2dcdf53dd4e8c357a76cd025
RJ <radoslawjurczak95@gmail.com>
2017-03-13 17:56:26 +0100

    Reporting module initial commit Added a (very) simple draft of the module responsible for reporting and analysis reconstruction.

---
+++ reporting/PipelineReader.py
@@ -0,0 +1,8 @@
+'''
+A reader for xml files representing Pytnam analyses, enabling to reconstruct the whole analysis step-by-step.
+'''
+
+
+class PipelineReader:
+    pass
+#TODO
---
+++ reporting/ReportWriter.py
@@ -0,0 +1,8 @@
+'''
+A class for automated analysis report generation.
+'''
+
+
+class ReportWriter:
+    pass
+#TODO
